3b08fd0c50e189f84d7b14bec3c8baa7b590b167
lucilegvl <lucile.gouvenel@mmibordeaux.com>
2022-03-01 16:58:50 +0100

    le truc de gwendal ne fonctionne pas

--- CONNECT/config.php
+++ CONNECT/config.php
@@ -11,3 +11,5 @@ $serverBD = "mysql:dbname=$nomBD;host=$hostBD;charset=utf8";
 $userBD = 'root';         // Votre login
 // mot de passe de connexion à la BDD
 $passBD = '';         // Votre Pass
+
+define('ROOT', $_SERVER['DOCUMENT_ROOT'] . '/BLOGART22');
