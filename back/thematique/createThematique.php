ab9e8c70b5b36f6e2778c81f6f18f0a27f467528
lucilegvl <lucile.gouvenel@mmibordeaux.com>
2022-03-02 12:53:10 +0100

    front create thematique pas trop moche

--- back/thematique/createThematique.php
+++ back/thematique/createThematique.php
@@ -18,6 +18,9 @@ require_once __DIR__ . '/../../CLASS_CRUD/Thematique.class.php';
 
 // Insertion classe Langue 
 require_once __DIR__ . '/../../CLASS_CRUD/langue.class.php';
+
+require_once ROOT . '/front/includes/commons/___headerFront.php';
+
 // Instanciation de la classe thématique
 $maThematique = new THEMATIQUE ();
 
@@ -90,91 +93,135 @@ include __DIR__ . '/initThematique.php';
     <link href="../css/style.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
-    <h1>BLOGART22 Admin - CRUD Thematique</h1>
-    <h2>Ajout d'une Thematique</h2>
-
-    <form method="POST" action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>" enctype="multipart/form-data" accept-charset="UTF-8">
-
-      <fieldset>
-        <legend class="legend1">Formulaire Thematique...</legend>
-
-        <input type="hidden" id="id" name="id" value="<?= isset($_GET['id']) ? $_GET['id'] : '' ?>" />
-
-        <div class="control-group">
-            <label class="control-label" for="libThem"><b>Libellé :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></label>
-            <input type="text" name="libThem" id="libThem" size="80" maxlength="80" value="<?= $libThem; ?>" placeholder="Titre de la thématique" autocomplete="on" autofocus="autofocus" />
-        </div>
-        <br>
-<!-- --------------------------------------------------------------- -->
-<!-- --------------------------------------------------------------- -->
-    <!-- FK : Langue -->
+    <h1>mon espace administrateur</h1>
     
-<!-- --------------------------------------------------------------- -->
-
-                <!-- Listbox langue  -->
+    <div class=parentback>
+        <div class=menu-back>
+            <nav>
+                <ul class="menuback-liens">
+                    <li class="menu-back-gererArticles">
+                        <a href="../article/article.php" class=articles>Gérer mes articles</a>
+                    </li>
+                    <li class="menu-back-gererLangues">
+                        <a href="../langue/langue.php" class=langues>Gérer mes langues</a>
+                    </li>
+                    <li class="menu-back-angles">
+                        <a href="../angle/angle.php" class=angles>Gérer mes angles</a>
+                    </li>
+                    <li class="menu-back-membres">
+                        <a href="../membre/membre.php" class=membres>Gérer mes membres</a>
+                    </li>
+                    <li class="menu-back-utilisateurs">
+                        <a href="../user/user.php" class=users>Gérer mes users</a>
+                    </li>
+                    <li class="menu-back-com">
+                        <a href="../comment/comment.php" class=comment>Gérer mes commentaires</a>
+                    </li>
+                    <li class="menu-back-likeart">
+                        <a href="../likeArt/likeArt.php" class=likeart>Gérer mes like</a>
+                    </li>
+                    <li class="menu-back-likecom">
+                        <a href="../likeCom/likeCom.php" class=likecom>Gérer mes like sur commentaires</a>
+                    </li>
+                    <li class="menu-back-statut">
+                        <a href="../statut/statut.php" class=stat>Gérer mes statuts</a>
+                    </li>
+                    <li class="menu-back-MotsCles">
+                        <a href="../motCle/MotsCle.php" class=Mc>Gérer mes mots clés</a>
+                    </li>
+                    <li class="menu-back-MotsCles">
+                        <a href="../thematique/thematique.php" class=them>Gérer mes thématiques</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
-           <!-- Listbox Langue 2è temps-->
-           <br>
-        <label for="LibTypLang" title="Sélectionnez la langue !">
-            <b>Quelle langue :&nbsp;&nbsp;&nbsp;</b>
-        </label>
-        <input type="hidden" id="idTypLang" name="idTypLang" value="<?= $numLang; ?>" />
-            <select size="1" name="TypLang" id="TypLang"  class="form-control form-control-create" title="Sélectionnez la langue !" > -->
-                <option value="-1">- - - Choisissez une langue - - -</option>
-<?php
-                $listNumLang = "";
-                $listLib1Lang = "";
+    
+        <div class=formulaire>  
+        <h2>Ajout d'une Thematique</h2>
+        <form method="POST" action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>" enctype="multipart/form-data" accept-charset="UTF-8">
 
-                $result = $maLangue->get_AllLanguesOrderByLib1Lang();
-                if($result){
-                    foreach($result as $row) {
-                        $listNumLang = $row["numLang"];
-                        $listLib1Lang = $row["lib1Lang"];
-?>
-                        <option value="<?= $listNumLang; ?>">
-                            <?= $listLib1Lang; ?>
-                        </option>
-<?php
-                    } // End of foreach
-                }   // if ($result)
-?>
-            </select>
+        <fieldset>
+            <legend class="legend1">Formulaire Thematique...</legend>
+
+            <input type="hidden" id="id" name="id" value="<?= isset($_GET['id']) ? $_GET['id'] : '' ?>" />
+
+            <div class="control-group">
+                <label class="control-label" for="libThem"><b>Libellé :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></label>
+                <input type="text" name="libThem" id="libThem" size="80" maxlength="80" value="<?= $libThem; ?>" placeholder="Titre de la thématique" autocomplete="on" autofocus="autofocus" />
+            </div>
+            <br>
+    <!-- --------------------------------------------------------------- -->
+    <!-- --------------------------------------------------------------- -->
+        <!-- FK : Langue -->
+        
+    <!-- --------------------------------------------------------------- -->
+
+                    <!-- Listbox langue  -->
             
-    <!-- FIN Listbox langue-->
-<!-- --------------------------------------------------------------- -->
-    <!-- FK : Langue -->
-<!-- --------------------------------------------------------------- -->
-<!-- --------------------------------------------------------------- -->
-        <div class="control-group">
-            <div class="error">
-<?php
-            if ($erreur) {
-                echo ($errSaisies);
-            }
-            else {
-                $errSaisies = "";
-                echo ($errSaisies);
-            }
-?>
-            </div>
-        </div>
+            <!-- Listbox Langue 2è temps-->
+            <br>
+            <label for="LibTypLang" title="Sélectionnez la langue !">
+                <b>Quelle langue :&nbsp;&nbsp;&nbsp;</b>
+            </label>
+            <input type="hidden" id="idTypLang" name="idTypLang" value="<?= $numLang; ?>" />
+                <select size="1" name="TypLang" id="TypLang"  class="form-control form-control-create" title="Sélectionnez la langue !" > -->
+                    <option value="-1">- - - Choisissez une langue - - -</option>
+    <?php
+                    $listNumLang = "";
+                    $listLib1Lang = "";
 
-        <div class="control-group">
-            <div class="controls">
-                <br><br>
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                <input type="submit" value="Initialiser" style="cursor:pointer; padding:5px 20px; background-color:lightsteelblue; border:dotted 2px grey; border-radius:5px;" name="Submit" />
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                <input type="submit" value="Valider" style="cursor:pointer; padding:5px 20px; background-color:lightsteelblue; border:dotted 2px grey; border-radius:5px;" name="Submit" />
-                <br>
+                    $result = $maLangue->get_AllLanguesOrderByLib1Lang();
+                    if($result){
+                        foreach($result as $row) {
+                            $listNumLang = $row["numLang"];
+                            $listLib1Lang = $row["lib1Lang"];
+    ?>
+                            <option value="<?= $listNumLang; ?>">
+                                <?= $listLib1Lang; ?>
+                            </option>
+    <?php
+                        } // End of foreach
+                    }   // if ($result)
+    ?>
+                </select>
+                
+        <!-- FIN Listbox langue-->
+    <!-- --------------------------------------------------------------- -->
+        <!-- FK : Langue -->
+    <!-- --------------------------------------------------------------- -->
+    <!-- --------------------------------------------------------------- -->
+            <div class="control-group">
+                <div class="error">
+    <?php
+                if ($erreur) {
+                    echo ($errSaisies);
+                }
+                else {
+                    $errSaisies = "";
+                    echo ($errSaisies);
+                }
+    ?>
+                </div>
             </div>
-        </div>
-      </fieldset>
-    </form>
-<?php
-require_once __DIR__ . '/footerThematique.php';
 
-require_once __DIR__ . '/footer.php';
+            <div class="control-group">
+                <div class="controls">
+                    <br><br>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    <input type="submit" value="Initialiser" style="cursor:pointer; padding:5px 20px; background-color:#0e1a27" name="Submit" />
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    <input type="submit" value="Valider" style="cursor:pointer; border-color: #0e1a27; padding:5px 20px; background-color:#0e1a27" name="Submit" />
+                    <br>
+                </div>
+            </div>
+        </fieldset>
+        </form>
+        </div>
+    </div>
+
+<?php
+require_once ROOT . '/front/includes/commons/___footerFront.php';
+
 ?>
 </body>
 </html>
